130b85e9b908c5b9e7303e15b661d5c8e2d01f33
georgiansof <georgian.sofronea@gmail.com>
2022-03-09 21:03:41 +0200

    fixed jagged camera movement

--- Moondev/scripts/main menu/main_bg.gd
+++ Moondev/scripts/main menu/main_bg.gd
@@ -7,7 +7,7 @@ extends Node2D
 
 var x_ratio := 16
 var y_ratio := 9
-var speed := 10
+var speed := 20
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,7 +16,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _process(delta):
 	self.position+=(Vector2.DOWN * y_ratio + Vector2.RIGHT * x_ratio ) * speed * delta
 	if self.position.x>=1280 || self.position.y>=720:
 		self.position=Vector2.ZERO
