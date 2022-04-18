cd1c740a455d4a0cfc257016ca05af2bef425f1d
georgiansof <georgian.sofronea@gmail.com>
2022-04-18 17:56:12 +0300

    asd

--- Moondev/scripts/NG_main.gd
+++ Moondev/scripts/NG_main.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var backstory = preload("res://scenes/vid_res/backstory.webm")
+var backstory_loaded := false
 
 func _ready():
 	$VideoPlayer.volume_db = linear2db(globals.music_volume/100.0)
@@ -12,11 +13,24 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("action_skip") && backstory_loaded==false:
+		$VideoPlayer.stop()
+		$VideoPlayer.emit_signal("finished")
+	pass
 
+func Load_Backstory() -> void:
+	$VideoPlayer.stream = backstory
+	$VideoPlayer.play()
+	backstory_loaded = true
+	
 
 func _on_VideoPlayer_finished():
-	var settings:= {"NG" : false}
-	globals.Save(globals.current_savegame,settings)
+	if backstory_loaded==true:
+		var settings:= {"NG" : false}
+		globals.Save(globals.current_savegame,settings)
+# warning-ignore:return_value_discarded
+		get_tree().change_scene("res://scenes/overworld.tscn")
+	else:
+		Load_Backstory()
 	pass 
